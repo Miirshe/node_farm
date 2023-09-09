a033f8971fc75961b258b1716a91e2bc29a07735
Miirshe <miirshe@gmail.com>
2023-09-09 22:08:54 +0300

    simple web app farm ecommerce

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>Simple Full Stack  </h1>
+<h4>Simple Full Stack Web Sales Fresh Fruits , Farm Vegetable </h4>
+<br>
+<h5>This Beauty Website building : </h5>
+<ul>
+<li>Back_End : Node_js</li>
+<li>Front_End
+<ul>
+<li>JavaScript</li>
+<li>HTML</li>
+<li>CSS</li>
+</ul>
+</li>
+</ul>
+<br>
+
+<h6>Output/ </h6>
---
+++ server/index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const url = require('url');
+const http = require('http');
+const path = require('path');
+const replaceTemplate = (temp, res) => {
+    let ouput = temp.replace(/{%productName%}/g, res.productName);
+    ouput = ouput.replace(/{%image%}/g, res.image);
+    ouput = ouput.replace(/{%from%}/g, res.from);
+    ouput = ouput.replace(/{%quantity%}/g, res.quantity);
+    ouput = ouput.replace(/{%nutrients%}/g, res.nutrients);
+    ouput = ouput.replace(/{%price%}/g, res.price);
+    ouput = ouput.replace(/{%description%}/g, res.description);
+    ouput = ouput.replace(/{%id%}/g, res.id);
+    if (!res.organic) ouput = ouput.replace(/{%not-organic%}/g, 'not-organic');
+
+    return ouput;
+}
+const tempOverview = fs.readFileSync(path.join('..', 'client', 'overview.html'), 'utf-8');
+const tempOverviewCard = fs.readFileSync(path.join('..', 'client', 'view_card.html'), 'utf-8');
+const tempProducts = fs.readFileSync(path.join('..', 'client', 'product.html'), 'utf-8');
+const data = fs.readFileSync(path.join(__dirname, 'model', '.', 'data.model.json', ), 'utf-8');
+const objectData = JSON.parse(data);
+const app = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
+
+    //Overview Page
+    if (pathname == '/' || pathname == '/overview') {
+        res.writeHead(200, {
+            'content-type': 'text/html',
+        })
+        const product_card = objectData.map(res => replaceTemplate(tempOverviewCard, res)).join('');
+        const ouput = tempOverview.replace('{%ProductCards%}', product_card);
+        res.end(ouput);
+    }
+
+    //Product Page
+    else if (pathname == '/product') {
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        const product = objectData[query.id];
+        const ouput = replaceTemplate(tempProducts, product);
+        res.end(ouput);
+    }
+
+    // Not Found Page
+    else {
+        res.writeHead(404, {
+            'content-type': 'text/html'
+        })
+        res.end("<h1>Page Not Found ! </h1>")
+    }
+})
+
+const PORT = 8800;
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
